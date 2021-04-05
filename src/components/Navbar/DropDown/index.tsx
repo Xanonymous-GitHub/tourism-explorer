83dcbe3a025db33763a5ddfd9c6230065b247b03
Xanonymous <trusaidlin@gmail.com>
2021-04-06 02:21:58 +0800

    nav bar prettier

--- src/components/Navbar/DropDown/index.tsx
+++ src/components/Navbar/DropDown/index.tsx
@@ -22,22 +22,22 @@ export const DropDown = (): JSX.Element => {
         <div className="px-10">
             <div className="dropdown inline-block relative">
                 <button className="bg-gray-300 text-gray-700 py-2 px-4 rounded inline-flex items-center">
-                    <span className="mr-1">{currentCity}</span>
+                    <span className="mr-1 whitespace-nowrap">{currentCity}</span>
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
                     </svg>
                 </button>
-                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1 max-h-96 overflow-scroll">
+                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1 max-h-96 overflow-scroll whitespace-nowrap">
                     {
                         cities.map(city =>
                             city.name !== currentCity ?
                                 <Link to={'/ScenicSpot/' + city.id} key={city.id}>
-                                    <li className="bg-gray-200 hover:bg-gray-400 active:bg-gray-500 py-2 px-4 block whitespace-no-wrap select-none cursor-pointer">
+                                    <li className="bg-gray-200 hover:bg-gray-400 active:bg-gray-500 py-2 px-4 block whitespace-no-wrap select-none cursor-pointer whitespace-nowrap">
                                         {city.name}
                                     </li>
                                 </Link> :
                                 <li key={city.id}
-                                    className="bg-gray-200 hover:bg-gray-400 active:bg-gray-500 py-2 px-4 block whitespace-no-wrap select-none cursor-pointer">
+                                    className="bg-gray-200 hover:bg-gray-400 active:bg-gray-500 py-2 px-4 block whitespace-no-wrap select-none cursor-pointer whitespace-nowrap">
                                     {city.name}
                                 </li>
                         )
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,32 +6,15 @@ export const Navbar = (): JSX.Element => {
         <nav className="bg-gray-800">
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <button type="button"
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                                aria-controls="mobile-menu" aria-expanded="false">
-                            <span className="sr-only">Open main menu</span>
-                            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                      d="M4 6h16M4 12h16M4 18h16"/>
-                            </svg>
-                            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                      d="M6 18L18 6M6 6l12 12"/>
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex-shrink-0 flex items-center">
-                            <img className="block lg:hidden h-8 w-auto"
-                                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"/>
-                            <img className="hidden lg:block h-8 w-auto"
-                                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start h-full relative">
+                        <div className="flex-shrink-0 flex items-center h-full relative">
+                            <img className="block md:hidden h-5/6 w-auto"
+                                 src="https://i.imgur.com/v2mpZLd.webp" alt="Workflow"/>
+                            <img className="hidden md:block h-5/6 relative w-auto"
+                                 src="https://i.imgur.com/alnkukQ.webp"
                                  alt="Workflow"/>
                         </div>
-                        <div className="hidden sm:block sm:ml-6">
+                        <div className="block sm:ml-6 my-auto">
                             <div className="flex space-x-4">
                                 <DropDown/>
                             </div>
